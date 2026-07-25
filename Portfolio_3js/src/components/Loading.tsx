96c61bae44b1b5ac0842d420885df08e3b6af9fc
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-07-26 00:11:06 +0530

    refactor: update personal branding in UI components and simplify README documentation

--- Portfolio_3js/src/components/Loading.tsx
+++ Portfolio_3js/src/components/Loading.tsx
@@ -62,8 +62,8 @@ const Loading = ({ percent }: { percent: number }) => {
       <div className="loading-screen">
         <div className="loading-marquee">
           <Marquee>
-            <span> A Creative Developer</span> <span>A Creative Designer</span>
-            <span> A Creative Developer</span> <span>A Creative Designer</span>
+            <span> AI Engineer</span> <span>A Developer</span>
+            <span> AI Engineer</span> <span>A Developer</span>
           </Marquee>
         </div>
         <div
